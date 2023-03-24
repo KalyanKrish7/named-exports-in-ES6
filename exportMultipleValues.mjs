a3a2a673a36436e05b0a602c154b6538cde4d5da
KalyanKrish7 <akaramkalyan7@gmail.com>
2023-03-24 10:01:07 +0000

    named exports using ES6 modules

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,33 @@
+// creating Array
+
+let myArray = ["camel", 265, true, "5.6"];
+
+// creating Object
+
+let bulb = {
+  watts: 10,
+  type: "LED",
+};
+
+// creating function
+
+let multiplyByFour = (num) => num * 4;
+
+export { myArray, bulb, multiplyByFour };
+
+// export using in ES6 named EXPORTS another way
+
+// // creating Array
+
+// export let myArray = ["camel", 265, true, "5.6"];
+
+// // creating Object
+
+// export let bulb = {
+//   watts: 10,
+//   type: "LED",
+// };
+
+// // creating function
+
+// export let multiplyByFour = (num) => num * 4;
